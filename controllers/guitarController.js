3724da47137a254c8f3c30267dbb55a591f9035a
Phoenix <psstout@gmail.com>
2023-09-21 17:07:04 -0700

    separate guitars and categories in mongoDB

--- controllers/guitarController.js
+++ controllers/guitarController.js
@@ -4,9 +4,12 @@ import asyncHandler from "express-async-handler";
 
 const index = asyncHandler(async (req, res, next) => {
   // Get details of books, book instances, authors and genre counts (in parallel)
- const data = await Guitar.find()
- console.log(data)
- res.render('index', {items:data})
+ const [guitars, categories] = await Promise.all([
+    Guitar.find().exec(),
+    Category.find().exec()
+ ])
+ console.log(categories)
+ res.render('index', {items:guitars, categories:categories})
 });
 
 export default index;
